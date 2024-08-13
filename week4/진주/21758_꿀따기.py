24af18da0e3d0461455ac63cf55a77a7974ebb44
Pearl <80253713+Pearl-K@users.noreply.github.com>
2024-08-13 18:16:54 +0900

    Update 21758_꿀따기.py

--- week4/진주/21758_꿀따기.py
+++ week4/진주/21758_꿀따기.py
@@ -12,6 +12,9 @@ for i in range(1, N+1):
     arr[i] = tmp[i-1]
     pre_s[i] = arr[i] + pre_s[i-1]
 
+# 벌 하나가 가장 왼쪽, 꿀통이 가장 오른쪽, 나머지 벌이 중간 어딘가
+# 벌 하나가 가장 오른쪽, 꿀통이 가장 왼쪽, 나머지 벌이 중간 어딘가
+# 벌이 모두 양쪽 끝, 꿀통이 중간 어딘가
 res = 0
 for i in range(2, N):
     res = max(res, pre_s[N] - arr[1] - arr[i] + pre_s[N] - pre_s[i])
